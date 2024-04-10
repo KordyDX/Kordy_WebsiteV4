64899d4f25423574975aea5af0aab12e80eef3d2
Kristián Stefurak <majk123yt@gmail.com>
2024-04-11 01:31:12 +0200

    Svelte Update

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import CardProfile from "./components/CardProfile.svelte";
+  import CardSection from "./components/CardSection.svelte";
+
+  function updateTheme() {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+  updateTheme();
+</script>
+
+<div class="flex flex-row">
+  <CardProfile/>
+  <CardSection/>
+</div>
---
+++ src/components/CardProfile.svelte
@@ -0,0 +1,44 @@
+<script>
+    import Separator from "$lib/components/ui/separator/separator.svelte";
+</script>
+
+<div class="text-black dark:text-white flex flex-col dark:bg-[#1e1e1f] bg-white">
+    <img src="" alt="">
+    <h1>Kristián Stefurak</h1>
+    <h2>Webový developer</h2>
+    <Separator/>
+    <div class="flex flex-col">
+        <div class="flex">
+            <a href="">
+                <img src="/email.svg" alt="" srcset="">
+            </a>
+            <div>
+                <h2>Email</h2>
+                <a href=""></a>
+            </div>
+        </div>
+        <div class="flex">
+            <a href="">
+                <img src="/github.svg" alt="" srcset="">
+            </a>
+            <div>
+                <h2>Github</h2>
+                <a href=""></a>
+            </div>
+        </div>
+        <div class="flex">
+            <a href="">
+                <img src="/marker.svg" alt="" srcset="">
+            </a>
+            <div>
+                <h2>Bydliště</h2>
+                <a href=""></a>
+            </div>
+        </div>
+        <div class="flex flex-row">
+            <a href="https://www.instagram.com/kristian_kordy/"><img src="/instagram.svg" alt=""></a>
+            <a href="https://discordapp.com/users/399983880548909057"><img src="/discord.svg" alt=""></a>
+            <a href="https://twitter.com/KristianNeAsi"><img src="/x.svg" alt=""></a>
+        </div>
+    </div>
+</div>
